f6f0498a2ee5e622eb6f67d17fa02760b5211188
rumia <rumia.youkai.of.dusk@gmail.com>
2011-12-11 15:19:18 +0700

    initial mini

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+define('EXT', '.php');
+define('DS', DIRECTORY_SEPARATOR);
+define('BASEPATH', realpath(dirname(__FILE__)).DS);
+define('CLASSDIR', 'classes');
+define('CLASSPATH', BASEPATH.CLASSDIR.DS);
+
+$host = (empty($_SERVER['HTTP_HOST']) ? '127.0.0.1' : $_SERVER['HTTP_HOST']);
+define('SYSTEM_HOST', $host);
+unset($host);
+
+require_once(CLASSPATH.'serac'.DS.'base'.EXT);
+require_once(CLASSPATH.'serac'.EXT);
+
+$system = new Serac;
+
+// add some routes here
+
+$system->run();

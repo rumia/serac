e9fae211970b61a4179e6ce475a9a5929dbe4e3d
rumia <rumia.youkai.of.dusk@gmail.com>
2012-03-28 12:16:42 +0700

    - switched to strpos for method validation - added $this->current_route for the current matched route - can call route handler statically - renamed:    - compile_path() to compiled_prefix_part()    - dispatch() to route() - domain can use wildcard similar to DNS record (eg: *.foo.com)

--- serac.php
+++ serac.php
@@ -8,7 +8,7 @@ class serac implements ArrayAccess
    const EXT               = '.php',
          VERSION           = '0.2',
          DEFAULT_METHOD    = 'get',
-         VALID_METHODS     = '/^(?:get|post|put|delete|head|options)$/';
+         VALID_METHODS     = '|get|post|put|delete|head|options|';
 
    public $protocol  = NULL;
    public $scheme    = NULL;
@@ -16,6 +16,7 @@ class serac implements ArrayAccess
    public $method    = self::DEFAULT_METHOD;
    public $uri       = NULL;
    public $routes    = array();
+   public $current_route;
    public $options   =
       array(
          'encoding'              => 'utf-8',
@@ -74,7 +75,7 @@ class serac implements ArrayAccess
          else $this->hostname = 'localhost';
 
          $method = strtolower(getenv('METHOD'));
-         if (preg_match(self::VALID_METHODS, $method))
+         if (strpos(self::VALID_METHODS, '|'.$method.'|'))
             $this->method = $method;
          else
             $this->method = self::DEFAULT_METHOD;
@@ -115,7 +116,7 @@ class serac implements ArrayAccess
             $method = $_POST['_method'];
 
          $method = strtolower($method);
-         if (preg_match(self::VALID_METHODS, $method))
+         if (strpos(self::VALID_METHODS, '|'.$method.'|'))
             $this->method = $method;
          else
             $this->method = self::DEFAULT_METHOD;
@@ -215,14 +216,14 @@ class serac implements ArrayAccess
 
    public function run()
    {
-      $found = $this->get_matching_route();
-      if (!empty($found))
+      $this->current_route = $this->get_matching_route();
+      if (!empty($this->current_route))
       {
          $pre_exec = $this->get_option('pre_exec');
          if (!empty($pre_exec) && is_callable($pre_exec))
-            call_user_func_array($pre_exec, array($this, $found));
+            call_user_func_array($pre_exec, array($this, $this->current_route));
 
-         $result = $this->execute($found);
+         $result = $this->execute($this->current_route);
 
          $post_exec = $this->get_option('post_exec');
          if (!empty($post_exec) && is_callable($post_exec))
@@ -251,19 +252,43 @@ class serac implements ArrayAccess
          {
             if (!empty($def['class']))
             {
-               $class   = $def['class'];
-               $object  = new $class($this);
+               $class      = $def['class'];
+               $classref   = new ReflectionClass($def['class']);
 
-               if (method_exists($object, $def['function']))
-                  $callback = array($object, $def['function']);
+               if (!$classref->hasMethod($def['function']))
+                  return FALSE;
+
+               $methodref = $classref->getMethod($def['function']);
+               if ($methodref->isPublic())
+               {
+                  if ($methodref->isStatic())
+                  {
+                     $callback = array($class, $def['function']);
+                     array_unshift($args, $this);
+                  }
+                  elseif (is_string($class) && $classref->isInstantiable())
+                  {
+                     $object = $classref->newInstance($this);
+                     $callback = array($object, $args);
+                  }
+               }
+               else return FALSE;
             }
             else
             {
-               array_unshift($args, $this);
-               if ($def['function'] instanceOf Closure ||
-                  (is_string($def['function']) &&
-                  function_exists($def['function'])))
+               if
+               (
+                  $def['function'] instanceOf Closure ||
+                  (
+                     is_string($def['function']) &&
+                     function_exists($def['function'])
+                  )
+               )
+               {
                   $callback = $def['function'];
+                  array_unshift($args, $this);
+               }
+               else return FALSE;
             }
          }
          else return FALSE;
@@ -291,26 +316,25 @@ class serac implements ArrayAccess
 
          if (preg_match($regex, $query, $matches))
          {
-            $captured = array_slice($matches, 4);
             if (is_array($def))
             {
-               if (count($captured) > 0)
+               if (count($matches) > 1)
                {
                   foreach ($def as $key => $value)
                   {
+                     if (!is_string($value)) continue;
                      $def[$key] = preg_replace(
-                        '/(?:\$([a-z][a-z0-9_]+))/ie',
-                        'isset($captured["$1"]) ? $captured["$1"] : ""',
+                        '/(?:\$([a-z0-9_]+))/ie',
+                        'isset($matches["$1"]) ? $matches["$1"] : ""',
                         $value);
                   }
                }
 
-               if (isset($def['arguments']))
+               if (isset($def['arguments']) && mb_strlen($def['arguments']))
                   $def['arguments'] = explode('/', $def['arguments']);
             }
 
             $this->routes[$glob] = $def;
-
             return $this->routes[$glob];
          }
       }
@@ -320,56 +344,62 @@ class serac implements ArrayAccess
 
    protected function compile_route($pattern)
    {
-      static $compiled = array();
-
-      if (!isset($compiled[$pattern]))
+      $regex = FALSE;
+      if (!preg_match('#^\{.*?\}$#', $pattern))
       {
-         if (!preg_match('#^\{.*?\}$#', $pattern))
+         $scheme = $host = $method = '';
+
+         if (preg_match('#^([^/]+)?(.*)$#', $pattern, $matches))
          {
-            $regex = FALSE;
-            $scheme = $host = $method = '';
+            $parts = explode(':', trim($matches[1], ':'));
 
-            if (preg_match('#^([^/]+)?(.*)$#', $pattern, $matches))
+            switch (count($parts))
             {
-               $parts = explode(':', trim($matches[1], ':'));
+               case 3:
+                  $method  = trim($parts[2]);
+                  $host    = trim($parts[1]);
+                  $scheme  = trim($parts[0]);
+                  break;
 
-               switch (count($parts))
-               {
-                  case 3:
-                     $method  = trim($parts[2]);
-                     $host    = trim($parts[1]);
-                     $scheme  = trim($parts[0]);
-                     break;
+               case 2:
+                  $method  = trim($parts[1]);
+                  $host    = trim($parts[0]);
+                  break;
 
-                  case 2:
-                     $method  = trim($parts[1]);
-                     $host    = trim($parts[0]);
-                     break;
-
-                  case 1:
-                     $method  = trim($parts[0]);
-                     break;
-               }
-
-               $scheme_re  = $this->compile_prefix_part($scheme);
-               $host_re    = $this->compile_prefix_part($host);
-               $method_re  = $this->compile_prefix_part($method);
-               $path_re    = $this->compile_path($matches[2]);
-
-               $regex = sprintf(
-                  '{^(%s):(%s):(%s):%s}',
-                  $scheme_re,
-                  $host_re,
-                  $method_re,
-                  $path_re);
+               case 1:
+                  $method  = trim($parts[0]);
+                  break;
             }
+
+            $scheme_re  = $this->compile_prefix_part($scheme);
+            $host_re    = $this->compile_prefix_part($host);
+            $method_re  = $this->compile_prefix_part($method);
+            $path_re    = $this->compile_path_part($matches[2]);
+
+            $regex = sprintf(
+               '{^(?:%s):(?:%s):(?:%s):%s}',
+               $scheme_re,
+               $host_re,
+               $method_re,
+               $path_re);
          }
-         else $regex = $pattern;
-
-         $compiled[$pattern] = $regex;
       }
+      else $regex = $pattern;
 
-      return $compiled[$pattern];
+      return $regex;
+   }
+
+   protected function compile_host_glob($pattern)
+   {
+      if (preg_match('/^((?:\*\.)+)?(.+)/', $pattern, $matches))
+      {
+         $regex =
+            str_replace('*.', '[^.]+\.', $matches[1]).
+            preg_quote($matches[2], '/');
+      }
+      else $regex = preg_quote(trim($pattern), '/');
+
+      return $regex;
    }
 
    protected function compile_prefix_part($part)
@@ -380,50 +410,41 @@ class serac implements ArrayAccess
          {
             $list = explode(',', $part);
             foreach ($list as $key => $value)
-               $list[$key] = preg_quote(trim($value), '/');
+               $list[$key] = $this->compile_host_glob($value);
 
             $regex = implode('|', $list);
          }
-         else $regex = preg_quote($part, '/');
+         else $regex = $this->compile_host_glob($part);
       }
       else $regex = '.+';
 
       return $regex;
    }
 
-   protected function compile_path($path)
+   protected function compile_path_part($path)
    {
-      $segments = explode('/', trim($path, '/'));
-      $res = array();
-
-      foreach ($segments as $segment)
+      $regex = '';
+      foreach (explode('/', trim($path, '/')) as $segment)
       {
          if ($segment === '*')
-         {
-            $res[] = '(?:/([^/]+))';
-         }
+            $regex .= '(?:/([^/]+))';
          elseif (strncmp($segment, '?', 1) === 0)
          {
-            $name = substr($segment, 1);
-            if (!preg_match('/^[a-z][a-z0-9_]*$/i', $name))
-               $res[] = '/'.preg_quote($segment);
+            if (preg_match('/^(?:\?([a-z][a-z0-9_]*))$/i', $segment, $matches))
+               $regex .= '(?:/(?<'.$matches[1].'>[^/]+))';
             else
-               $res[] = '(?:/(?<'.$name.'>[^/]+))';
+               $regex .= '/'.preg_quote($segment);
          }
          elseif ($segment === '~$')
-         {
-            $res[] = '(?:/?(.*))?';
-         }
+            $regex .= '(?:/?(.*))?';
          else
-         {
-            $res[] = '/'.preg_quote($segment);
-         }
+            $regex .= '/'.preg_quote($segment);
       }
 
-      return implode('', $res);
+      return $regex;
    }
 
-   public function dispatch($pattern, $handler = NULL)
+   public function route($pattern, $handler = NULL)
    {
       if (is_array($pattern))
       {
@@ -445,9 +466,9 @@ class serac implements ArrayAccess
    public function offsetSet($index, $value)
    {
       if ($index === NULL)
-         $this->dispatch($value);
+         $this->route($value);
       else
-         $this->dispatch($index, $value);
+         $this->route($index, $value);
    }
 
    public function offsetExists($index)
